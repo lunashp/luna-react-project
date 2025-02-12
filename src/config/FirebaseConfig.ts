ea36a28765e5482f3b2686d66b06a24514b2f280
lunashp <hijnshp@gmail.com>
2025-02-12 17:02:30 +0900

    feat: Firebase Firestore 설정

--- src/config/FirebaseConfig.ts
+++ src/config/FirebaseConfig.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { getAuth } from "@firebase/auth";
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,4 +17,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firebaseAuth = getAuth(app);
+export const firebaseAuth = getAuth(app);
+export const db = getFirestore(app);
