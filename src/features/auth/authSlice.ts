20a7de15984536f22c48793699278c4d619af27d
lunashp <hijnshp@gmail.com>
2025-02-13 14:38:37 +0900

    feat: authSlice user에 password 추가

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface AuthState {
-  user: { uid: string; email: string } | null;
+  user: { uid?: string; email?: string; password?: string } | null;
   isAuthenticated: boolean;
 }
 
@@ -13,11 +13,16 @@ const initialState: AuthState = {
 /**
  * Auth Slice 생성
  */
+// todo: login 타입 확인 후 명시 필요
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    login: (state, action: PayloadAction<{ uid: string; email: string }>) => {
+    login: (
+      state,
+      action: PayloadAction<{ uid: string; email: string; password: string }>
+    ) => {
+      // login: (state, action) => {
       state.user = action.payload;
       state.isAuthenticated = true;
     },
