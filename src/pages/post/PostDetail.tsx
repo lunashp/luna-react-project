78f7a8ce6343761ad97baeb188fba9a483eb4239
lunashp <hijnshp@gmail.com>
2025-02-17 17:12:10 +0900

    fix: 불필요한 코드 삭제

--- src/pages/post/PostDetail.tsx
+++ src/pages/post/PostDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAppSelector } from "../../stores/hooks/storeHooks";
 
