c955119aa31924e66bdd1ef69dad316db9e5fa69
lunashp <hijnshp@gmail.com>
2025-02-18 16:02:50 +0900

    fix: 게시글 수정 함수 수정

--- src/pages/post/PostDetail.tsx
+++ src/pages/post/PostDetail.tsx
@@ -1,24 +1,43 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { useAppSelector } from "../../stores/hooks/storeHooks";
+import { useAppDispatch, useAppSelector } from "../../stores/hooks/storeHooks";
+import { deletePost } from "../../features/posts/postSlice";
 
 const PostDetail = () => {
   const { id } = useParams();
+  const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const posts = useAppSelector((state) =>
     state.posts.posts.find((post) => post.id === id)
   );
 
-  console.log("id", id);
-  console.log("post", posts);
+  const userId = useAppSelector((state) => state.auth.user?.uid);
+  console.log("postID", posts?.authorId);
+  console.log("userId", userId);
+
+  /**
+   * 현재 로그인한 사용자 정보(firbaseAuth.currentUser)를 가져오는 방법
+   */
+  // const currentUser = firebaseAuth.currentUser; // 현재 로그인한 사용자
 
   const handleGoToPostList = () => {
     navigate("/post");
   };
 
-  //todd: 본인 게시글만 수정할 수 있게끔
   const handleGoToUpdate = () => {
     navigate(`/post/${id}/update`);
+  };
+
+  // 게시글 삭제 함수
+  const handleDeletePost = async (postId: string) => {
+    try {
+      await dispatch(deletePost(postId)).unwrap();
+      alert("게시글이 삭제되었습니다.");
+      navigate("/post");
+    } catch (error) {
+      alert("게시글 삭제에 실패했습니다.");
+      console.error(error);
+    }
   };
 
   return (
@@ -28,7 +47,12 @@ const PostDetail = () => {
       title: {posts?.title}
       content: {posts?.content}
       <button onClick={handleGoToPostList}>게시글 목록으로 이동</button>
-      <button onClick={handleGoToUpdate}>게시글 수정</button>
+      {posts?.authorId === userId && (
+        <>
+          <button onClick={handleGoToUpdate}>게시글 수정</button>
+          <button onClick={() => handleDeletePost(posts?.id!)}>삭제</button>
+        </>
+      )}
     </>
   );
 };
--- src/pages/post/PostList.tsx
+++ src/pages/post/PostList.tsx
@@ -1,31 +1,18 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { firebaseAuth } from "../../config/FirebaseConfig";
 import { useAppDispatch, useAppSelector } from "../../stores/hooks/storeHooks";
-import { deletePost, fetchPosts } from "../../features/posts/postSlice";
+import { fetchPosts } from "../../features/posts/postSlice";
 
 const PostList = () => {
   const dispatch = useAppDispatch();
   const posts = useAppSelector((state) => state.posts.posts);
   const navigate = useNavigate();
-  const currentUser = firebaseAuth.currentUser; // 현재 로그인한 사용자
 
   // todo: useSelector 로 loading 상태 추가
 
   useEffect(() => {
     dispatch(fetchPosts());
   }, [dispatch]);
-
-  // 게시글 삭제 함수
-  const handleDeletePost = async (postId: string) => {
-    try {
-      await dispatch(deletePost(postId)).unwrap();
-      alert("게시글이 삭제되었습니다.");
-    } catch (error) {
-      alert("게시글 삭제에 실패했습니다.");
-      console.error(error);
-    }
-  };
 
   return (
     <div>
@@ -36,11 +23,6 @@ const PostList = () => {
           <li key={post.id}>
             <h3 onClick={() => navigate(`/post/${post.id}`)}>{post.title}</h3>
             <p>작성자: {post.authorDisplayName}</p>
-            {currentUser?.uid === post.authorId && (
-              <>
-                <button onClick={() => handleDeletePost(post.id)}>삭제</button>
-              </>
-            )}
           </li>
         ))}
       </ul>
