013f3ab680c485be9357e12dfde012c84cc3d7d5
lunashp <hijnshp@gmail.com>
2025-02-22 00:26:31 +0900

    fix: 게시판 목록 스타일 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,3 +11,14 @@ export const ThemeButton = styled(Button)({
   //   borderRadius: "30px",
   borderRadius: "12px",
 });
+
+export const BoardButton = styled(Button)({
+  backgroundColor: themeColors.accent,
+  color: "white",
+  "&:hover": {
+    backgroundColor: themeColors.text,
+  },
+  padding: "8px 65px",
+  borderRadius: "22px",
+  // borderRadius: "12px",
+});
--- src/pages/post/PostList.tsx
+++ src/pages/post/PostList.tsx
@@ -2,8 +2,25 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import postStore from "../../stores/features/posts/postStore";
 import { observer } from "mobx-react-lite";
-import { Pagination } from "@mui/material";
-// import Pagination from "./Pagination";
+import { Grid2, Pagination, styled, Typography } from "@mui/material";
+import { BoardButton, ThemeButton } from "../../components/Button";
+
+const Box = styled("div")`
+  margin-top: 30px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const BoardBox = styled("div")`
+  border: 1.5px solid #a67c52;
+  background-color: #fffef9;
+  padding: 10px;
+  margin: 10px 0;
+  cursor: pointer;
+  border-radius: 12px;
+`;
 
 const PostList = observer(() => {
   const navigate = useNavigate();
@@ -25,30 +42,32 @@ const PostList = observer(() => {
   }
 
   return (
-    <div>
-      <h1>게시판</h1>
-      <button onClick={() => navigate("/post/create")}>새 게시글 작성</button>
+    <Box>
+      <Grid2 container gap={6} justifyContent={"space-between"}>
+        <Typography variant="h4">Board</Typography>
+        {/* <button onClick={() => navigate("/post/create")}>새 게시글 작성</button> */}
+        <BoardButton onClick={() => navigate("/post/create")}>
+          새 게시글 작성
+        </BoardButton>
+      </Grid2>
+      {/* <h1>게시판</h1> */}
+      {/* <button onClick={() => navigate("/post/create")}>새 게시글 작성</button> */}
       {paginatedPosts.map((post) => (
-        <div
-          key={post.id}
-          style={{
-            border: "1px solid gray",
-            padding: "10px",
-            margin: "10px 0",
-          }}
-        >
-          <h3
-            onClick={() => navigate(`/post/${post.id}`)}
-            style={{ cursor: "pointer" }}
-          >
-            {post.title}
-          </h3>
+        <BoardBox key={post.id} onClick={() => navigate(`/post/${post.id}`)}>
+          <Typography variant="h6">{post.title}</Typography>
+          {/* <h3>{post.title}</h3> */}
           <p>{post.content}</p>
-          <p>작성일: {new Date(post.date!).toLocaleString()}</p>{" "}
-          <button onClick={() => handleDeletePost(post.id!)}>삭제</button>
-        </div>
+          <p>작성일: {new Date(post.date!).toLocaleString()}</p>
+          <button
+            onClick={(e) => {
+              e.stopPropagation(); // 부모 클릭 방지
+              handleDeletePost(post.id);
+            }}
+          >
+            삭제
+          </button>
+        </BoardBox>
       ))}
-
       {/* 페이지네이션*/}
       <Pagination
         count={totalPages} // 총 페이지 수
@@ -58,7 +77,7 @@ const PostList = observer(() => {
         showLastButton
         sx={{ display: "flex", justifyContent: "center", mt: 2 }}
       />
-    </div>
+    </Box>
   );
 });
 
