24d94ac28a28c3f5125012963e4c6dc4d794b24c
lunashp <hijnshp@gmail.com>
2025-02-23 17:33:22 +0900

    fix: 게시물 상세 스타일 수정

--- src/pages/post/PostDetail.tsx
+++ src/pages/post/PostDetail.tsx
@@ -3,6 +3,22 @@ import { useNavigate, useParams } from "react-router-dom";
 import useFile from "../../hooks/useFile";
 import postStore from "../../stores/features/posts/postStore";
 import authStore from "../../stores/features/auth/authStore";
+import styled from "styled-components";
+import { Button, Grid2, TextField, Typography } from "@mui/material";
+import { ThemeButton } from "../../components/Button";
+import { CloudDownload } from "@mui/icons-material";
+
+const Box = styled("div")`
+  margin-top: 60px;
+  text-align: center;
+  /* display: flex; */
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  width: 600px;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 const PostDetail = () => {
   const { id } = useParams();
@@ -50,27 +66,99 @@ const PostDetail = () => {
   };
 
   return (
-    <>
-      <div>PostDetail</div>
-      postId: {posts?.id}
-      title: {posts?.title}
-      content: {posts?.content}
-      {storedFile && (
-        <>
-          <div>fileName: {fileName}</div>
-          <button onClick={() => handleDownloadFile(storedFile, fileName)}>
-            파일 다운로드
-          </button>
-        </>
-      )}
-      <button onClick={handleGoToPostList}>게시글 목록으로 이동</button>
-      {posts?.authorId === userId && (
-        <>
-          <button onClick={handleGoToUpdate}>게시글 수정</button>
-          <button onClick={() => handleDeletePost(posts?.id!)}>삭제</button>
-        </>
-      )}
-    </>
+    <Box>
+      <Typography variant="h4" sx={{ mb: 6 }}>
+        게시물 상세
+      </Typography>
+      <Grid2 container alignItems="center" spacing={2} mb={4}>
+        <Grid2 sx={{ flex: 2 }}>
+          <Typography color="textSecondary">제목</Typography>
+        </Grid2>
+
+        <Grid2 sx={{ flex: 10 }}>
+          <TextField
+            type="text"
+            value={posts?.title}
+            variant="outlined"
+            fullWidth
+            disabled
+          />
+        </Grid2>
+      </Grid2>
+      <Grid2 container alignItems="center" spacing={2} mb={4}>
+        <Grid2 sx={{ flex: 2 }}>
+          <Typography color="textSecondary">내용</Typography>
+        </Grid2>
+
+        <Grid2 sx={{ flex: 10 }}>
+          <TextField
+            type="text"
+            value={posts?.content}
+            variant="outlined"
+            multiline
+            rows={9}
+            fullWidth
+            disabled
+          />
+        </Grid2>
+      </Grid2>
+
+      <Grid2 container gap={2} alignItems="center" mb={12}>
+        <Grid2 sx={{ flex: 2 }}>
+          <Typography color="textSecondary">파일 첨부</Typography>
+        </Grid2>
+        <Grid2
+          sx={{ flex: 10 }}
+          display="flex"
+          alignItems="center"
+          gap={1}
+          justifyContent={"space-between"}
+        >
+          {storedFile ? (
+            <>
+              <Typography>📎 {fileName}</Typography>
+              <Button
+                component="label"
+                color="success"
+                role={undefined}
+                variant="outlined"
+                tabIndex={-1}
+                startIcon={<CloudDownload />}
+                onClick={() => handleDownloadFile(storedFile, fileName)}
+              >
+                파일 다운로드
+              </Button>
+            </>
+          ) : (
+            <Typography color="textSecondary">첨부파일 없음</Typography>
+          )}
+        </Grid2>
+      </Grid2>
+
+      <Grid2
+        container
+        gap={2}
+        alignItems="center"
+        mb={12}
+        justifyContent={"space-between"}
+      >
+        <ThemeButton onClick={handleGoToPostList}>
+          게시글 목록으로 이동
+        </ThemeButton>
+        {posts?.authorId === userId && (
+          <>
+            <ThemeButton onClick={handleGoToUpdate}>게시글 수정</ThemeButton>
+            <Button
+              onClick={() => handleDeletePost(posts?.id!)}
+              color="warning"
+              variant="outlined"
+            >
+              삭제
+            </Button>
+          </>
+        )}
+      </Grid2>
+    </Box>
   );
 };
 export default PostDetail;
